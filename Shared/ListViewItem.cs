b3e9ab1d920116dab2510bf9fcd87e585ff45679
ms.zojaji <ms.zojaji@gmail.com>
2017-12-08 17:52:08 +0330

    Left and Right Slide sectiones are added to the ListViewItem.

--- Shared/ListViewItem.cs
+++ Shared/ListViewItem.cs
@@ -11,15 +11,22 @@ namespace Zebble
 
     public class ListViewItem : Canvas
     {
-        bool SlideVisible = false;
-        float OriginalContentX;
+        bool RightSlideVisible, LeftSlideVisible, ShouldRelease, ShouldTapBegin;
+        float? OriginalContentX;
+
         public TimeSpan SwipeAnimationDuration = Animation.DefaultListItemSlideDuration;
         public readonly Stack Content = new Stack { Direction = RepeatDirection.Horizontal, Id = "Content" };
 
-        public readonly Stack SlideIn = new Stack
+        public readonly Stack RightSlideIn = new Stack
         {
             Direction = RepeatDirection.Horizontal,
-            Id = "SlideIn"
+            Id = "RightSlideIn"
+        }.Absolute();
+
+        public readonly Stack LeftSlideIn = new Stack
+        {
+            Direction = RepeatDirection.Horizontal,
+            Id = "LeftSlideIn"
         }.Absolute();
 
         public override async Task OnInitializing()
@@ -41,8 +48,25 @@ namespace Zebble
         {
             var direction = await CheckDirection(args.Velocity);
             if (direction == null) return;
-            if (direction == Direction.Left) await ShowSlideInContent();
-            else if (direction == Direction.Right) await HideSlideInContent();
+
+            var distance = args.From.X - args.To.X;
+            if (direction == Direction.Left) await ShowRightSlideInContent(distance);
+            else if (direction == Direction.Right) await ShowLeftSlideInContent(distance);
+        }
+
+        async Task OnPanFinished(PannedEventArgs args)
+        {
+            if (ShouldRelease)
+            {
+                RightSlideVisible = LeftSlideVisible = true;
+                await HideSlideInContent();
+            }
+
+            if (ShouldTapBegin && (RightSlideVisible || LeftSlideVisible))
+            {
+                //TODO Default functionality should be execute.
+                ShouldTapBegin = false;
+            }
         }
 
         Task<Direction?> CheckDirection(Point velocity)
@@ -58,39 +82,118 @@ namespace Zebble
         public override async Task OnPreRender()
         {
             await base.OnPreRender();
-            if (SlideIn.AllChildren.Any()) Panning.Handle(OnPanning);
+
+            Panning.Handle(OnPanning);
+            PanFinished.Handle(OnPanFinished);
         }
 
-        async Task ShowSlideInContent()
+        async Task ShowRightSlideInContent(double distance)
         {
-            if (SlideVisible) return;
-            else SlideVisible = true;
+            if (RightSlideVisible) return;
+            else if (LeftSlideVisible) { await HideSlideInContent(); return; }
 
-            if (SlideIn.parent == null)
+            if (!RightSlideIn.AllChildren.Any()) return;
+
+            if (RightSlideIn.AllChildren.Count == 1)
             {
-                SlideIn.Size(100.Percent()).X(CalculateAbsoluteX() + ActualWidth);
-                await base.AddAt(1, SlideIn, awaitNative: true); // Lazy loaded the first time only.
+                if (Content.X.CurrentValue <= -(Content.ActualWidth / 2))
+                {
+                    ShouldTapBegin = RightSlideVisible = true;
+                    return;
+                }
+                else ShouldRelease = true;
+            }
+            else if (RightSlideIn.AllChildren.Count > 1 && RightSlideIn.parent != null)
+            {
+                var slideInWidth = RightSlideIn.CurrentChildren.Sum(c => c.ActualWidth);
+                if (RightSlideIn.X.CurrentValue <= (CalculateAbsoluteX() + ActualWidth - slideInWidth) - Padding.Right())
+                {
+                    RightSlideVisible = true;
+                    ShouldRelease = false;
+                    return;
+                }
+                else ShouldRelease = true;
             }
 
-            var slideInWidth = SlideIn.CurrentChildren.Sum(c => c.ActualWidth);
-            OriginalContentX = Content.ActualX;
+            //First time only.
+            if (RightSlideIn.parent == null)
+            {
+                RightSlideIn.Size(100.Percent()).X(CalculateAbsoluteX() + ActualWidth);
+                await base.AddAt(AllChildren.Count, RightSlideIn, awaitNative: true); // Lazy loaded the first time only.
+                if (OriginalContentX == null) OriginalContentX = Content.ActualX;
+            }
 
-            await Task.WhenAll(
-                Content.Animate(SwipeAnimationDuration, x => x.X(-slideInWidth)),
-                SlideIn.Animate(SwipeAnimationDuration, x => x.X((CalculateAbsoluteX() + ActualWidth - slideInWidth) - Padding.Right()))
-            );
+
+            Content.X(Content.X.CurrentValue - distance);
+            RightSlideIn.X(RightSlideIn.X.CurrentValue - distance);
+        }
+
+        async Task ShowLeftSlideInContent(double distance)
+        {
+            if (LeftSlideVisible) return;
+            else if (RightSlideVisible) { await HideSlideInContent(); return; }
+
+            if (!LeftSlideIn.AllChildren.Any()) return;
+            float slideInWidth;
+
+            if (LeftSlideIn.AllChildren.Count == 1)
+            {
+                if (Content.X.CurrentValue >= Content.ActualWidth / 2)
+                {
+                    ShouldTapBegin = LeftSlideVisible = true;
+                    return;
+                }
+                else ShouldRelease = true;
+            }
+            else if (LeftSlideIn.AllChildren.Count > 1 && LeftSlideIn.parent != null)
+            {
+                slideInWidth = LeftSlideIn.CurrentChildren.Sum(c => c.ActualWidth);
+                if (LeftSlideIn.X.CurrentValue >= 0)
+                {
+                    LeftSlideVisible = true;
+                    ShouldRelease = false;
+                    return;
+                }
+                else ShouldRelease = true;
+            }
+
+            //First time only.
+            if (LeftSlideIn.parent == null)
+            {
+                await base.AddAt(AllChildren.Count, LeftSlideIn, awaitNative: true); // Lazy loaded the first time only.
+                if (OriginalContentX == null) OriginalContentX = Content.ActualX;
+
+                slideInWidth = LeftSlideIn.CurrentChildren.Sum(c => c.ActualWidth);
+                LeftSlideIn.Size(100.Percent()).X(-(slideInWidth + Padding.Left()));
+            }
+
+            Content.X(Content.X.CurrentValue - distance);
+            LeftSlideIn.X(LeftSlideIn.X.CurrentValue - distance);
         }
 
         async Task HideSlideInContent()
         {
-            if (!SlideVisible) return;
-            else SlideVisible = false;
+            if (LeftSlideVisible && RightSlideVisible) await Task.WhenAll(HideLeftSlideInContent(), HideRightSlideInContent());
+            if (LeftSlideVisible) await HideLeftSlideInContent();
+            else if (RightSlideVisible) await HideRightSlideInContent(); 
 
+            ShouldRelease = RightSlideVisible = LeftSlideVisible = false;
+        }
 
+        async Task HideRightSlideInContent()
+        {
             await Task.WhenAll(
                 Content.Animate(SwipeAnimationDuration, x => x.X(OriginalContentX)),
-                SlideIn.Animate(SwipeAnimationDuration, x => x.X(Device.Screen.Width))
-            );
+                RightSlideIn.Animate(SwipeAnimationDuration, x => x.X(CalculateAbsoluteX() + ActualWidth)));
+        }
+
+        async Task HideLeftSlideInContent()
+        {
+            var slideInWidth = LeftSlideIn.CurrentChildren.Sum(c => c.ActualWidth);
+
+            await Task.WhenAll(
+            Content.Animate(SwipeAnimationDuration, x => x.X(OriginalContentX)),
+            LeftSlideIn.Animate(SwipeAnimationDuration, x => x.X(-slideInWidth)));
         }
 
         public override async Task<TView> Add<TView>(TView child, bool awaitNative = false) => await Content.Add(child, awaitNative);
@@ -98,22 +201,25 @@ namespace Zebble
         public override async Task<TView> AddAt<TView>(int index, TView child, bool awaitNative = false)
         {
             index--; // Content
-            if (SlideIn.parent != null) index--; // SlideIn
+            if (RightSlideIn.parent != null || LeftSlideIn.parent != null) index--; // SlideIn
             return await Content.AddAt(index, child, awaitNative).DropContext();
         }
 
-        public override bool CanCancelTouches => SlideVisible;
+        public override bool CanCancelTouches => RightSlideVisible || LeftSlideVisible;
 
         public override void RaiseTapped(TouchEventArgs args)
         {
-            if (SlideVisible) HideSlideInContent().RunInParallel();
+            if (RightSlideVisible || LeftSlideVisible) HideSlideInContent().RunInParallel();
             else base.RaiseTapped(args);
         }
 
         public override void Dispose()
         {
-            if (SlideIn?.IsDisposed == false)
-                SlideIn?.Dispose();
+            if (RightSlideIn?.IsDisposed == false)
+                RightSlideIn?.Dispose();
+
+            if (LeftSlideIn?.IsDisposed == false)
+                LeftSlideIn?.Dispose();
 
             base.Dispose();
         }
